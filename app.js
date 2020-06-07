1d0c9cb914b8ab8f49ae6451862f98e94ae91086
Pivato <giovanipivato@gmail.com>
2020-06-07 18:52:11 -0300

    Further adjustments before deploy

--- app.js
+++ app.js
@@ -155,6 +155,11 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(process.env.PORT, function () {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
   console.log("Server has started sucessfully!");
 });
