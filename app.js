8379fb731f0b9136f69c6f2caca1c8ebe0e56f71
Pivato <giovanipivato@gmail.com>
2020-06-07 18:56:15 -0300

    Test2

--- app.js
+++ app.js
@@ -155,17 +155,10 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.get("/about1", function (req, res) {
-  res.render("about");
-});
-
-
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-
-//Test.
 
 app.listen(port, function () {
   console.log("Server has started sucessfully!");
